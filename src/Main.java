efbc9d0ab48dd85eca9a0b25a043490652651050
zhanat-B-skaeng <zhanat07021986@gmail.com>
2025-08-15 02:51:53 +1100

    Выполнено ДЗ-7

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,80 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Conditional operator2");
+        System.out.println("Задача 1");
+
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
+
+        System.out.println("Задача 2");
+
+        clientOS = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        }
+
+        System.out.println("Задача 3");
+        int year = 1984;
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) ||year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+
+        }
+
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int days = 1;
+
+        if (deliveryDistance <=20) {
+            System.out.println("Потребуется дней:" + 1);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней:" + 2);
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней:" + 3);
+        } else {
+            System.out.println("Доставки нет");
+
+        }
+
+        System.out.println("Задача 5");
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Весна");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Завершение выполнения!");
+        }
     }
 }
